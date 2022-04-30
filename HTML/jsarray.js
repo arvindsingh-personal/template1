731b4108dde57101dec94bdd33552b3b8e5b29b9
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2022-04-30 14:01:40 +0530

    Drop Down to sort products

--- HTML/jsarray.js
+++ HTML/jsarray.js
@@ -30,30 +30,99 @@
     });
 
     
+    
+   var Select;
+   var Item;
+   var txt;
+   Select = document.getElementById("dropdown").value;
    
+
+   function tableView(){
+    Item =document.getElementById("searchedItem").value;
+    txt = "<table><tr><th>Company</th><th>Model</ht><th>Memory</th><th>Price</th></tr>";
+    for(let i=0 ; i<=4 ; i++){
+        
+            if(products[i][Select] == Item){
+                
+                txt += "<tr>";
+                for(let k=0 ; k<=3 ; k++){
+                var x = products[i][k];
+                txt += "<td>" + x + "</td>";
+                }
+                txt += "</tr>";
+
+            
+        }
+    }
+  txt += "</table>"; 
+  }
+
      function search(){
 
-        var Select = document.getElementById("dropdown").value;
-        var Item =document.getElementById("searchedItem").value;
+        
 
         console.log(Select);
         console.log(Item);
-    
-
-         var txt = "<table><tr><th>Company</th><th>Model</ht><th>Memory</th><th>Price</th></tr>";
-          for(let i=0 ; i<=4 ; i++){
-              for(let j = Select ; j <= Select ; j++){
-                  if(products[i][j] == Item){
-                      txt += "<tr>";
-                      for(let k=0 ; k<=3 ; k++){
-                      var x = products[i][j];
-                      txt += "<td>" + x + "</td>";
-                      }
-                      txt += "</tr>";
-                  }
-              }
-          }
-        txt += "</table>";
+        tableView();
+        console.log(txt);
+        
         document.getElementById("div1").innerHTML = txt;
      }
+     
+function tableSortedView(){
+    txt = "<table><tr><th>Company</th><th>Model</ht><th>Memory</th><th>Price</th></tr>";
+    
+    for(let i=0 ; i<=4 ; i++){
+        txt+="<tr>";
+        for(let j=0;j<4;j++){
+            
+                txt += "<td>"+products[i][j]+"</td>";
+               
+
+            
+        }
+        txt+="</tr>";
+
+    }
+  txt += "</table>"; 
+  document.getElementById("div2").innerHTML = txt;
+
+      }
+    
+     
+     function sorting(){
+
+     const dropD2 = document.getElementById("dropdown2").value;
+     const dropD3 = document.getElementById("dropdown3").value;
+
+     console.log(dropD2);
+     console.log(dropD3);
+
+     if(dropD2 == 0){
+
+        products.sort(sortFunction);
+     
+        function sortFunction(a,b){
+            if(a[dropD3] == b[dropD3])
+              return 0;
+            else 
+              return (a[dropD3] > b[dropD3]) ? 1 : -1;
+        }
+     }
+     
+     else{
+
+        products.sort(sortFunction);
+     
+        function sortFunction(a,b){
+           if(a[dropD3] == b[dropD3])
+             return 0;
+           else 
+             return (a[dropD3] > b[dropD3]) ? -1 : 1;
+        }
        
+     }
+     
+     tableSortedView();
+     //
+    }
