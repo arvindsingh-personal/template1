9f4b01efd3c8271dac5520b2fbb6bb7bc14c1af3
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2022-09-16 19:41:56 +0530

    Object

---
+++ CreateObject.js
@@ -0,0 +1,9 @@
+function Position() {
+  alert(document.getElementById('position').value);
+}
+function Type() {
+  alert(document.getElementById('type').value);
+}
+function Availability() {
+  alert(document.getElementById('availability').value)
+}
