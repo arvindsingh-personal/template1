69fe8ca3f26e185d1d3496bd60b3a96533d855f2
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2022-09-19 13:36:28 +0530

    JavaScript Object Methods

--- CreateObject.js
+++ CreateObject.js
@@ -1,7 +1,15 @@
 let position;
 let type;
 let available;
-let emp = {};
+let emp = { position: String, type: String, availability: String };
+let eat = { name: String, category: String, avail: String }
+let Employee;
+let Food;
+let keyArray = [];
+let valueArray = [];
+let name1;
+let category;
+let avail;
 function Position() {
   position = document.getElementById('position').value;
 }
@@ -11,13 +19,80 @@ function Type() {
 function Availability() {
   available = document.getElementById('availability').value;
 }
-function Print() {
-  console.log(position + " " + type + " " + available);
-  emp = { position: position, type: type, availability: available }
-  showDetails();
+//Create the Employee Object
+function Create() {
+  Employee = Object.create(emp);
+  Employee.position = position;
+  Employee.type = type;
+  Employee.availability = available;
+  console.log(Employee);
+}
+// Prints the Employee Object
+function showDetails() {
+  document.getElementById('demo').innerHTML = Employee.position + " " + Employee.type + " " + Employee.availability;
+}
+// Prevent the Employee object from updation
+function Freez() {
+  Object.freeze(Employee);
+}
+//Update the Emplpyee Object
+function Update() {
+  Employee.position = position;
+  Employee.type = type;
+  Employee.availability = available;
+}
+//count the number of keys in Employee Object
+function printKeys() {
+  let num = 0;
+  for (const [key, value] of Object.entries(Employee)) {
+    num++;
+    keyArray.push(key);
+    valueArray.push(value);
+  }
+  document.getElementById('keyNum').append(num);
+  displayKey();
+  displayValue();
+}
+//Print the keys of Employee Object
+function displayKey() {
+  document.getElementById('keys').append(keyArray);
+}
+//Print the values of Employee Object
+function displayValue() {
+  document.getElementById('values').append(valueArray);
+}
+//Print the array containing array of each key-value pair as elements
+function printArray() {
+  let arr = Object.entries(Employee);
+  console.log(arr)
+  document.getElementById('demo1').innerHTML = arr;
 }
 
-function showDetails() {
-  document.getElementById('demo').innerHTML = emp.position + " " + emp.type + " " + emp.availability;
-  let Employee = Object.create(emp);
+function Name() {
+  name1 = document.getElementById('name').value;
+}
+function Category() {
+  category = document.getElementById('category').value;
+}
+function Available() {
+  avail = document.getElementById('available').value;
+}
+//Crea the Food Object
+function Create1() {
+  Food = Object.create(eat);
+  Food.name = name1;
+  Food.category = category;
+  Food.avail = avail;
+  console.log(Food);
+}
+//Merges the Food and Employee Objects
+function Merge() {
+  const mergedObject = Object.assign(Food, Employee);
+  console.log(mergedObject);
+  let table = '<table border=1><tr><th>Key</th><th>Value</th></tr>';
+  for (const [key, value] of Object.entries(mergedObject)) {
+    table += `<tr><td>${key}</td><td>${value}</td></tr>`
+  }
+  table += '</table>';
+  document.getElementById('demo2').innerHTML = table;
 }
